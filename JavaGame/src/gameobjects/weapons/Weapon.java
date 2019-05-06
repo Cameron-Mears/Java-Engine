f4a63aeee2e6c29ab2f586e1e46240ecf4e8ad8c
Cameron-Mears <47492133+Cameron-Mears@users.noreply.github.com>
2019-05-05 23:22:03 -0700

    up

--- JavaGame/src/gameobjects/weapons/Weapon.java
+++ JavaGame/src/gameobjects/weapons/Weapon.java
@@ -1,17 +1,37 @@
 package gameobjects.weapons;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import game.Game;
+import game.Gamecore;
 import graphics.Sprite;
 
 public abstract class Weapon
 {
+    protected static BufferedImage[] images;
     protected double damage;
     protected AmmoTypes ammoType;
     protected int currentAmmo;
     protected int magSize;
     protected double fireRate;
+    protected double fireDelay;
+    protected double fireWait;
     protected double accuarcy;
     protected double reloadSpeed;
     protected Sprite sprite;
+
+    public Weapon(double fireRate)
+    {
+        this.fireRate = fireRate;
+        this.fireDelay = 1000/fireRate;
+        this.fireWait = fireDelay;
+    }
+    public AmmoTypes getAmmoType()
+    {
+        return ammoType;
+    }
+
 
     public double getDamage() 
     {
@@ -31,5 +51,17 @@ public abstract class Weapon
     public void setMagsize(int magSize) 
     {
         this.magSize = magSize;
+    }
+
+    public boolean canFire()
+    {
+        if(fireWait <= 0) return true;
+        fireWait -= Game.deltaMS;
+        return false;        
+    }
+
+    public void render(Graphics g)
+    {
+        
     }
 }
